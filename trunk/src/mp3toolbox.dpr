9c59622b6e9b6a6407d18b87ffc664018ae64fe8
mark.ziegler@rakekniven.de <mark.ziegler@rakekniven.de@de726b56-a236-28d7-7239-e99a5a77749f>
2011-07-19 14:58:17 +0000

    Added function to use ID3 tag values instead of filename. Used Mp3FileUtils to achieve this. Have to check licence before commiting lib code. Checkbox "With pathes" is not needed anymore

--- trunk/src/mp3toolbox.dpr
+++ trunk/src/mp3toolbox.dpr
@@ -10,7 +10,10 @@ uses
   U_Language in 'U_Language.pas',
   U_Main in 'U_Main.pas' {F_Main},
   U_Setup in 'U_Setup.pas' {F_Setup},
-  U_About in 'U_About.pas' {About_F};
+  U_About in 'U_About.pas' {About_F},
+  U_CharCode in 'externallibs\U_CharCode.pas',
+  Id3v2Frames in 'externallibs\Id3v2Frames.pas',
+  Mp3FileUtils in 'externallibs\Mp3FileUtils.pas';
 
 {$R *.res}
 
