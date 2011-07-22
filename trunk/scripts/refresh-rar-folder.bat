97a77ab75a9c4ede285303096223305859136b90
mark.ziegler@rakekniven.de <mark.ziegler@rakekniven.de@de726b56-a236-28d7-7239-e99a5a77749f>
2011-07-22 16:45:35 +0000

    Adjusted script to build setup correctly

--- trunk/scripts/refresh-rar-folder.bat
+++ trunk/scripts/refresh-rar-folder.bat
@@ -1,7 +1,12 @@
 mkdir ..\setup\rar-folder
 mkdir ..\setup\rar-folder\LC
+mkdir ..\setup\rar-folder\config
+mkdir ..\setup\rar-folder\templates
 
 copy ..\src\mp3toolbox.exe ..\setup\rar-folder
-copy ..\src\Lang*.txt ..\setup\rar-folder
+copy ..\src\LC\Lang*.txt ..\setup\rar-folder\LC
+copy ..\src\config\*.cfg ..\setup\rar-folder\config
+copy ..\src\templates\*.txt ..\setup\rar-folder\templates
+copy ..\src\templates\*.html ..\setup\rar-folder\templates
 
 pause ...
