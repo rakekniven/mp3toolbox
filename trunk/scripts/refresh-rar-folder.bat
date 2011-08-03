667e8dad8fb31a75d3cd2bb6a418cc4258351dcd
mark.ziegler@rakekniven.de <mark.ziegler@rakekniven.de@de726b56-a236-28d7-7239-e99a5a77749f>
2011-08-03 18:39:04 +0000

    VERY IMPORTANT: Forgot to add gpl licence Here it is!

--- trunk/scripts/refresh-rar-folder.bat
+++ trunk/scripts/refresh-rar-folder.bat
@@ -4,6 +4,7 @@ mkdir ..\setup\rar-folder\config
 mkdir ..\setup\rar-folder\templates
 
 copy ..\src\mp3toolbox.exe ..\setup\rar-folder
+copy ..\src\gpl-2.0.txt ..\setup\rar-folder
 copy ..\src\LC\Lang*.txt ..\setup\rar-folder\LC
 copy ..\src\config\*.cfg ..\setup\rar-folder\config
 copy ..\src\templates\*.txt ..\setup\rar-folder\templates
